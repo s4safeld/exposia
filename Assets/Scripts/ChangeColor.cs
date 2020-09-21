22ab3f6b77fff17a589ab6dfdf6b7ac16af00e20
Sascha Feld <sascha.feld@gmx.net>
2020-09-21 18:57:46 +0200

    added first demo scene, player character still missing

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Windows;
+
+public class ChangeColor : MonoBehaviour {
+    private Camera cam;
+    public int color;
+    public SpriteRenderer background;
+    
+    // Start is called before the first frame update
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void OnMouseDown() {
+        if (color == 0) {
+            background.color = Color.black;
+        }
+        else {
+            background.color = Color.white;
+        }
+
+        Information.Color = color;
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+    private int globalColor;
+    public int color;
+    // Start is called before the first frame update
+    void Start() {
+        globalColor = Information.Color;
+        if (globalColor == color) {
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<Collider2D>().enabled = false;
+        }
+        else {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GetComponent<Collider2D>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (globalColor != Information.Color) {
+            globalColor = Information.Color;
+            if (globalColor == color) {
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<Collider2D>().enabled = false;
+            }
+            else {
+                GetComponent<SpriteRenderer>().enabled = true;
+                GetComponent<Collider2D>().enabled = true;
+            }
+            
+        }
+    }
+}
