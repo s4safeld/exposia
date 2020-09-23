d8244e0369d521ebfeef6bb70a903e164bc98b93
Garrrrrrrek <koevkoev@hotmail.de>
2020-09-23 02:05:18 +0200

    Player Controller

---
+++ Assets/MouseMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveX;
+    private float moveY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        moveX = 0;
+        moveY = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            moveX += Input.GetAxis("Mouse X");
+        }
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            moveY += Input.GetAxis("Mouse Y");
+        }
+
+
+        rb.MovePosition(new Vector2(transform.position.x + moveX, transform.position.y + moveY));
+        moveX = 0;
+        moveY = 0;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask lm;
+    private Rigidbody2D rb;
+    public BoxCollider2D bc2;
+    private float moveX;
+    private float moveY;
+    public float walkSpeed;
+    public float fallSpeed;
+    public float jumpSpeed;
+    public bool jumping;
+    public int jumpTimer;
+    public int maxJumpTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        bc2 = this.GetComponent<BoxCollider2D>();
+        moveX = 0;
+        moveY = 0;
+        /*grounded = false;
+        jumping = false;*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX += walkSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX -= walkSpeed;
+        }
+
+        if (!Grounded())
+        {
+            moveY -= fallSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && AlmostGrounded())
+        {
+            jumping = true;
+            jumpTimer = maxJumpTimer;
+        }
+        if (jumping)
+        {
+            jumpTimer--;
+            moveY += jumpSpeed;
+        }
+        if (jumpTimer == 0)
+        {
+            jumping = false;
+        }
+
+        rb.MovePosition(new Vector2(transform.position.x + moveX, transform.position.y + moveY));
+        moveX = 0;
+        moveY = 0;
+    }
+
+    private bool AlmostGrounded()
+    {
+        RaycastHit2D groundRay = Physics2D.BoxCast(bc2.bounds.center, bc2.bounds.size, 0f, Vector2.down, 0.5f, lm);
+
+        return groundRay.collider != null;
+    }
+
+    private bool Grounded()
+    {
+        RaycastHit2D groundRay = Physics2D.BoxCast(bc2.bounds.center, bc2.bounds.size, 0f, Vector2.down, 0.1f, lm);
+
+        return groundRay.collider != null;
+    }
+
+    /*void OnTriggerEnter2D(Collider2D other)
+    {
+        grounded = true;
+        //transform.position = new Vector2(transform.position.x, transform.position.y + 5);
+        //Debug.Log("Collision!");
+    }*/
+}
